004401a3aaf0acdb08f36cc39407d42bf1eb5581
Seth Woodworth <seth@sethish.com>
2018-04-29 08:38:49 -0400

    Adds syllables method which returns accurite counts.

--- pyku/pyku.py
+++ pyku/pyku.py
@@ -1,3 +1,24 @@
-# -*- coding: utf-8 -*-
+"""Turns sentences into haiku."""
 
-"""Main module."""
+from pyphen import Pyphen
+
+def count_syllables(word):
+    return max(1, len(Pyphen(lang='en_US').hd.positions(word)) + 1)
+
+def syllables(word):
+    #referred from stackoverflow.com/questions/14541303/count-the-number-of-syllables-in-a-word
+    count = 0
+    vowels = 'aeiouy'
+    word = word.lower()
+    if word[0] in vowels:
+        count += 1
+    for index in range(1,len(word)):
+        if word[index] in vowels and word[index-1] not in vowels:
+            count +=1
+    if word.endswith('e'):
+        count -= 1
+    if word.endswith('le'):
+        count+=1
+    if count == 0:
+        count +=1
+    return count
